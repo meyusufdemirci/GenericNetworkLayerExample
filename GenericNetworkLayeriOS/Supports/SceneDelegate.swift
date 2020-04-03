b7bbb8fe6cbd50857fa4c29ba414f521b1919717
Yusuf Demirci <demirciy94@gmail.com>
2020-04-03 22:13:06 +0300

    - Initialized project with HomeController.

--- GenericNetworkLayeriOS/Supports/SceneDelegate.swift
+++ GenericNetworkLayeriOS/Supports/SceneDelegate.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // MARK: - Properties
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = HomeController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ GenericNetworkLayeriOS/UI/HomeController.swift
@@ -0,0 +1,11 @@
+//
+//  HomeController.swift
+//  GenericNetworkLayeriOS
+//
+//  Created by Yusuf Demirci on 3.04.2020.
+//  Copyright © 2020 Yusuf Demirci. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {}
